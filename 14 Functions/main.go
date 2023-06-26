c4b9562e330e9dc8dccc7a9537310fc3f6e59857
Ayush Jain <ajsa02812@gmail.com>
2023-06-26 09:42:50 +0530

    go

--- 14 Functions/main.go
+++ 14 Functions/main.go
@@ -1,4 +1,4 @@
-package main
+gitpackage main
 
 import "fmt"
 
@@ -8,14 +8,32 @@ func main() {
 	greet2()
 	result := adder(3,5)
 	fmt.Println("result is:",result)
+
+	pror := proAdder(2,3,5,6,4)
+	fmt.Println("pro resullt is:",pror)
 }
+
+
 func adder(valone int, valtwo int)int{
 	return valone + valtwo
-
 }
+
+
+func proAdder(values...int)int{
+	total :=0
+
+	for _, val:= range values{
+		total +=val		
+	}
+	return total
+}
+
+
 func greet() {
 	fmt.Println("Hello, Ayush this side!!")
 }
+
+
 func greet2(){
 	fmt.Println("Another method")
 }

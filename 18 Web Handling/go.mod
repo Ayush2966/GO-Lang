b94a22912d66837a6313c29bdcbdea03e4d4d65f
Ayush Jain <ajsa02812@gmail.com>
2023-06-28 09:52:09 +0530

    go

---
+++ 18 Web Handling/go.mod
@@ -0,0 +1,3 @@
+module web-handling
+
+go 1.19
---
+++ 18 Web Handling/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+const url="https://lco.dev"
+
+func main() {
+	fmt.Println("My web request")
+
+	response, err := http.Get(url)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Printf("Response is of type: %T \n",response)
+	defer response.Body.Close()
+		//caller's responsibiliy to close the connection 
+	databytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	content := string(databytes)
+	fmt.Println(content)
+	}

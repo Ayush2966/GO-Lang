541da4266b6fa60dd6403543e33f9b647b2acc0a
Ayush Jain <ajsa02812@gmail.com>
2023-06-28 10:35:20 +0530

    go

---
+++ 19 handling url/go.mod
@@ -0,0 +1,3 @@
+module url-handling
+
+go 1.19
---
+++ 19 handling url/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghbj456ghb"
+
+func main() {
+	fmt.Println("welcome to handling url in golang")
+	fmt.Println(myurl)
+	//parsing
+	result, _ := url.Parse(myurl)
+	// fmt.Println(result.Scheme)
+	// fmt.Println(result.Host)
+	// fmt.Println(result.Path)
+	// fmt.Println(result.Port())
+	fmt.Println(result.RawQuery)
+
+	qparams := result.Query()
+	fmt.Printf("The type of Query params are %T \n", qparams)
+	fmt.Println(qparams["coursename"])
+	fmt.Println(qparams["paymentid"])
+
+	for _, val:= range qparams{
+		fmt.Println("Param is:", val)
+	}
+
+	partofUrl := &url.URL{
+		Scheme: "https",
+		Host : "lco.dev",
+		Path : "/tutcss",
+		RawPath: "user=hitesh",
+	}
+	anotherURl :=partofUrl.String()	
+	fmt.Println(anotherURl)
+}
